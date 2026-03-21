c9ce2dbc196820a745f1996a5c7ea8223bc8152e
komalsonwane06 <komalsonwane142@gmail.com>
2026-03-21 23:30:58 +0530

    Added new code

---
+++ Data Type Ranges.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    printf("short: -32768 to 32767 \n");
+    printf("int: -2147483648 to 2147483647 \n");
+    printf("long: -9223372036854775808 to 9223372036854775807 \n");
+    printf("long long: -9223372036854775808 to 9223372036854775807 \n");
+    printf("unsigned short: 0 to 65535 \n");
+    printf("unsigned int: 0 to 4294967295 \n");
+    printf("unsigned long: 0 to 18446744073709551615 \n");
+    printf("unsigned long long: 0 to 18446744073709551615 \n");
+    printf("char: -128 to 127 \n");
+    printf("unsigned char: 0 to 255 \n");
+    printf("float: 1.175494e-38 to 3.402823e+38 \n");
+    printf("double: 2.225074e-308 to 1.797693e+308 \n");
+    printf("long double: 3.362103e-4932 to 1.189731e+4932");
+    
+    
+    return 0;
+}
