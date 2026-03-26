83de74d624c185c9cda2e68a865366881d789806
komalsonwane06 <komalsonwane142@gmail.com>
2026-03-26 22:15:16 +0530

    code

---
+++ Loops_Multiplication Table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=10;++i){
+        printf("%d x %d = %d",n,i,n*i);
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
