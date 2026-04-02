462cfb9b6ec621c8e4a3d1d6bfbe696c50f42285
komalsonwane06 <komalsonwane142@gmail.com>
2026-04-02 22:06:59 +0530

    code

---
+++ Arrays_Find the Index of the Last Occurrence of a Given Element.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n,k;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; ++i){
+        scanf("%d", &a[i]);
+    }
+    scanf("%d", &k);
+    
+    int flag=-1;
+    for(int i=0; i<n; ++i){
+        if(k==a[i])
+            flag=i;
+    }
+        printf("%d",flag);
+    return 0;
+}
