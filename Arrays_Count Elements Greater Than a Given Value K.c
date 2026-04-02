0cedab3daea20f3d51b388a10aa4cbdc57da1d50
komalsonwane06 <komalsonwane142@gmail.com>
2026-04-02 22:08:26 +0530

    code

---
+++ Arrays_Count Elements Greater Than a Given Value K.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n,k;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; ++i)
+        scanf("%d", &a[i]);
+    scanf("%d",&k);
+    
+    int count=0;
+    for(int i=0; i<n; ++i){
+        if(a[i]>k)
+            ++count;
+    }
+    printf("%d",count);
+    
+    return 0;
+}
